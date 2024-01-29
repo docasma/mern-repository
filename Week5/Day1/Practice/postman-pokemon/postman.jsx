b0fac5f2015a79161d76e9dec87d772ff6e14163
docasma <asmabenhasine@yahoo.fr>
2024-01-29 12:40:35 +0100

    first commit

---
+++ Week5/Day1/Practice/postman-pokemon/postman.jsx
@@ -0,0 +1,157 @@
+// https://pokeapi.co/api/v2/pokemon/pikachu
+// {
+//     "abilities": [
+//         {
+//             "ability": {
+//                 "name": "static",
+//                 "url": "https://pokeapi.co/api/v2/ability/9/"
+//             },
+//             "is_hidden": false,
+//             "slot": 1
+//         },
+//         {
+//             "ability": {
+//                 "name": "lightning-rod",
+//                 "url": "https://pokeapi.co/api/v2/ability/31/"
+//             },
+//             "is_hidden": true,
+//             "slot": 3
+//         }
+//     ],
+//     "base_experience": 112,
+//     "forms": [
+//         {
+//             "name": "pikachu",
+//             "url": "https://pokeapi.co/api/v2/pokemon-form/25/"
+//         }
+//     ],
+
+
+// https://pokeapi.co/api/v2/pokemon/33
+// {
+//     "abilities": [
+//         {
+//             "ability": {
+//                 "name": "poison-point",
+//                 "url": "https://pokeapi.co/api/v2/ability/38/"
+//             },
+//             "is_hidden": false,
+//             "slot": 1
+//         },
+//         {
+//             "ability": {
+//                 "name": "rivalry",
+//                 "url": "https://pokeapi.co/api/v2/ability/79/"
+//             },
+//             "is_hidden": false,
+//             "slot": 2
+//         },
+//         {
+//             "ability": {
+//                 "name": "hustle",
+//                 "url": "https://pokeapi.co/api/v2/ability/55/"
+//             },
+//             "is_hidden": true,
+//             "slot": 3
+//         }
+//     ],
+//     "base_experience": 128,
+//     "forms": [
+//         {
+//             "name": "nidorino",
+//             "url": "https://pokeapi.co/api/v2/pokemon-form/33/"
+//         }
+//     ],
+
+
+// https://pokeapi.co/api/v2/pokemon/80
+// {
+//     "abilities": [
+//         {
+//             "ability": {
+//                 "name": "oblivious",
+//                 "url": "https://pokeapi.co/api/v2/ability/12/"
+//             },
+//             "is_hidden": false,
+//             "slot": 1
+//         },
+//         {
+//             "ability": {
+//                 "name": "own-tempo",
+//                 "url": "https://pokeapi.co/api/v2/ability/20/"
+//             },
+//             "is_hidden": false,
+//             "slot": 2
+//         },
+//         {
+//             "ability": {
+//                 "name": "regenerator",
+//                 "url": "https://pokeapi.co/api/v2/ability/144/"
+//             },
+//             "is_hidden": true,
+//             "slot": 3
+//         }
+//     ],
+//     "base_experience": 172,
+//     "forms": [
+//         {
+//             "name": "slowbro",
+//             "url": "https://pokeapi.co/api/v2/pokemon-form/80/"
+//         }
+//     ],
+
+// https://pokeapi.co/api/v2/pokemon/mew
+//     {
+//         "abilities": [
+//             {
+//                 "ability": {
+//                     "name": "synchronize",
+//                     "url": "https://pokeapi.co/api/v2/ability/28/"
+//                 },
+//                 "is_hidden": false,
+//                 "slot": 1
+//             }
+//         ],
+//         "base_experience": 300,
+//         "forms": [
+//             {
+//                 "name": "mew",
+//                 "url": "https://pokeapi.co/api/v2/pokemon-form/151/"
+//             }
+//         ],  
+        
+// https://pokeapi.co/api/v2/pokemon/105        
+//         {
+//             "abilities": [
+//                 {
+//                     "ability": {
+//                         "name": "rock-head",
+//                         "url": "https://pokeapi.co/api/v2/ability/69/"
+//                     },
+//                     "is_hidden": false,
+//                     "slot": 1
+//                 },
+//                 {
+//                     "ability": {
+//                         "name": "lightning-rod",
+//                         "url": "https://pokeapi.co/api/v2/ability/31/"
+//                     },
+//                     "is_hidden": false,
+//                     "slot": 2
+//                 },
+//                 {
+//                     "ability": {
+//                         "name": "battle-armor",
+//                         "url": "https://pokeapi.co/api/v2/ability/4/"
+//                     },
+//                     "is_hidden": true,
+//                     "slot": 3
+//                 }
+//             ],
+//             "base_experience": 149,
+//             "forms": [
+//                 {
+//                     "name": "marowak",
+//                     "url": "https://pokeapi.co/api/v2/pokemon-form/105/"
+//                 }
+//             ],
